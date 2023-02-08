d68981770bb0485fb223e9f3b7a7e46310e01273
Dmytro Shumsky <schooldima1@gmail.com>
2023-02-08 09:14:46 +0200

    dafault commit

---
+++ validate_board.py
@@ -0,0 +1,25 @@
+"""
+Puzzle Game
+"""
+
+board = [
+ "**** ****",
+ "***1 ****",
+ "**  3****",
+ "* 4 1****",
+ "     9 5 ",
+ " 6  83  *",
+ "3   1  **",
+ "  8  2***",
+ "  2  ****"
+]
+
+
+def validate_board(board):
+    """
+    Main function to check if board is valid
+    """
+    pass
+
+
+print(validate_board(board))
