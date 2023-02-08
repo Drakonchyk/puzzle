224d0f6784fb9c388f546db1a823012928e705d8
Dmytro Shumsky <schooldima1@gmail.com>
2023-02-08 10:13:40 +0200

    first validate color commit

--- validate_board.py
+++ validate_board.py
@@ -16,12 +16,28 @@ def validate_column(board):
     """
     pass
 
-def validate_color(board):
+def validate_color(board: list) -> bool:
     """
     check nums in colors on board
     returns bool
+    >>> validate_color("**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 6  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****")
+    True
     """
-    pass
+    row_board = [list(i) for i in board]
+    column_board = [[row_board[i][j] for i in range(len(row_board))]
+                     for j in range(len(row_board[0]))]
+    check_new_rows = ["".join(row_board[index]) + "".join(column_board[-(index+1)])
+                      for index in range(len(row_board[0]))]
+
+    return validate_row(check_new_rows)
 
 def validate_board(board):
     """
