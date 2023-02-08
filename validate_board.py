a9df61282a417427b0aee5385c5e4ae2ee1d8550
Andrii Hryshchenko <hryshchenko.pn@ucu.edu.ua>
2023-02-08 12:22:13 +0200

    Andrii Commit

--- validate_board.py
+++ validate_board.py
@@ -6,8 +6,32 @@ def validate_row(board):
     """
     check rows on board
     returns bool
+    >>> validate_row(["**** ****",\
+ "***1 ****",\
+ "**  3****",\
+ "* 4 1****",\
+ "     9 5 ",\
+ " 3  83  *",\
+ "3   1  **",\
+ "  8  2***",\
+ "  2  ****"])
+    False
+    >>> validate_row(["**** ****",\
+ "***1 ****",\
+ "**  2****",\
+ "* 4 1****",\
+ "     9 6 ",\
+ " 5  84  *",\
+ "3   2  **",\
+ "  7  2***",\
+ "  2  ****"])
+    True
     """
-    pass
+    for i in board:
+        for j in i:
+            if j.isdigit() and i.count(j) > 1:
+                return False
+    return True
 
 def validate_column(board):
     """
